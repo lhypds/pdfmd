3c66db4108b005d3c3830488ed9817f66ca55d03
Heyang Liu <lhypds@gmail.com>
2025-04-19 19:02:14 +0900

    Fix cleanup

--- clean.bat
+++ clean.bat
@@ -1,6 +1,7 @@
 @echo off
 REM Clean up generated PDF, Markdown, and PNG files in the current directory
-del /Q "*.pdf"
+del /Q "*_pdfmd.pdf"
+del /Q "*_pdfcrop.pdf"
 REM Delete markdown files except README.md
 FOR %%F IN (*.md) DO (
     IF /I NOT "%%~nxF"=="README.md" DEL /Q "%%F"
